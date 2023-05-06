85be4924042938b7d0a091d7021271bc93e4962b
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2023-05-05 21:02:50 -0700

    Updated config file.

--- application/config/config.php
+++ application/config/config.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 date_default_timezone_set('America/Tijuana');
 
-$config['base_url'] = 'http://127.0.0.1:8888';
-#$config['base_url'] = 'http://localhost/pos_system/pizzeria';
+//$config['base_url'] = 'http://127.0.0.1:8888';
+$config['base_url'] = 'http://localhost/pos_system/pizzeria';
 #$config['base_url'] = 'http://leanquattro.com/pos/pizzeria';
 
 
@@ -57,8 +57,8 @@ $config['sess_cookie_name'] = 'ci_session';
 $config['sess_samesite'] = 'Lax';
 $config['sess_expiration'] = 7200;
 
-//$config['sess_save_path'] = NULL;
-$config['sess_save_path'] = sys_get_temp_dir();
+$config['sess_save_path'] = NULL;
+//$config['sess_save_path'] = sys_get_temp_dir();
 
 
 $config['sess_match_ip'] = FALSE;
